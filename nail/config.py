a756f9059a141985a9230c341156093a000ac8bd
jacksonneal <jacksontneal@gmail.com>
2023-08-27 14:00:23 -0400

    switch default xgb strat

--- nail/config.py
+++ nail/config.py
@@ -18,7 +18,7 @@ class Settings(BaseSettings):
     data_version: str = "v4.1"
     feature_set: str = "small"
     models_dir: str = "replace-me!"
-    xgb_tree_method: str = "approx"
+    xgb_tree_method: str = "hist"
 
     @property
     def version_data_dir(self):
